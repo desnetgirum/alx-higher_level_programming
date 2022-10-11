811ef5aa4ca7e0bf3d783d6e60a223b7b3cdcca5
desnetgirum <desbe2020@gmail.com>
2022-10-11 15:04:11 -0700

    added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""File defines a square"""
-
-
 class Square:
-    """A class Square that defines a square"""
-    
-    def __init__(self, size=None):
-        """Initializes private attribute size """
-        self.__size = 
+    def __init__(self, size):
+        self.__size = size
