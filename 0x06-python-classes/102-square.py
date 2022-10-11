8e0350e664bc533a80f737ad72410fc2f18afab4
desnetgirum <desbe2020@gmail.com>
2022-10-11 16:40:22 -0700

    final

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -9,16 +9,16 @@ class Square:
     def __init__(self, size=0):
         """Initializes attribute size """
         self.size = size
-    
+
     def area(self):
         """Calculate area of square"""
-            return (self.__size * self.__size)
-    
+        return (self.__size * self.__size)
+
     @property
     def size(self):
         """Getter for square"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Initializes attribute size """
@@ -27,27 +27,27 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def __eq__(self, other):
         """Equal"""
         return self.size == other.size
-    
+
     def __ne__(self, other):
         """Not Equal"""
         return self.size != other.size
-    
+
     def __lt__(self, other):
         """Less than"""
         return self.size < other.size
-    
+
     def __le__(self, other):
         """Less than or equal"""
         return self.size <= other.size
-    
+
     def __gt__(self, other):
         """Greater than"""
-        return self.size > other.size    
-    
+        return self.size > other.size
+
     def __ge__(self, other):
         """Greater than or equal"""
         return self.size >= other.size
