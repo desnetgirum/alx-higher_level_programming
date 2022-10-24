d57554a86c6c89ea570cf229eb56e09f0ba1259d
desnetgirum <desbe2020@gmail.com>
2022-10-24 11:30:50 -0700

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,7 @@ Contains the class BaseGeometry and subclass Rectangle
 Rectangle = __import__('9-rectangle').Rectangle
 
 
+
 class Square(Rectangle):
     """A representation of a square"""
     def __init__(self, size):
